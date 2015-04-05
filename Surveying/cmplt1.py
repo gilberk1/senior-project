fb9edce668d853d5b262d3c5ffb529b7599255b3
dialloa1 <dialloa1@tcnj.edu>
2015-04-05 19:51:27 -0400

    Update cmplt1.py

--- Surveying/cmplt1.py
+++ Surveying/cmplt1.py
@@ -6,3 +6,13 @@ import time
 import threading
 import json,httplib
 import serial
+
+#pointing to the gps module
+sudo killall gpsd
+sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
+
+fo = open("mainfile", "w+")
+do = open("log", "r")
+
+gpsd = None 
+os.system('clear')
